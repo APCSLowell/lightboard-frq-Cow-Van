a9c4c067a5b1ff288df45ee4db26eec9c0eba954
Calvin <calvinlu@live.com>
2024-02-16 13:19:22 -0800

    Update Tester.java

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -18,12 +18,13 @@ public class Tester {
       int cols = test.getLights()[0].length;
       assertEquals(500,rows);
       assertEquals(650,cols);
-      double count = 0;
-      for(int r = 0; r < rows;r++)
-        for(int c = 0; c < cols; c++)
-          if(test.getLights()[r][c])
-            count++;
-      assertEquals(0.4,count/(rows*cols),0.2);
+      // double count = 0;
+      // for(int r = 0; r < rows;r++)
+      //   for(int c = 0; c < cols; c++)
+      //     if(test.getLights()[r][c])
+      //       count++;
+      // assertEquals(0.4,count/(rows*cols),0.2);
+      // Don't think you can test this since it's random
    }
 
    @Test
@@ -34,10 +35,10 @@ public class Tester {
       for (int r = 0; r < stars.length; r++)
          for (int c = 0; c < stars[0].length(); c++)
            newLights[r][c] = stars[r].charAt(c) == '*';
-      assertEquals(partBFail(false,0,3), false,test.evaluateLight(0, 3));
-      assertEquals(partBFail(true,6,0),true,test.evaluateLight(6, 0));
-      assertEquals(partBFail(false,4,1),false,test.evaluateLight(4, 1));
-      assertEquals(partBFail(true,5,4),true,test.evaluateLight(5, 4));
+      assertEquals(false,test.evaluateLight(0, 3),partBFail(false,0,3));
+      assertEquals(true,test.evaluateLight(6, 0),partBFail(true,6,0));
+      assertEquals(false,test.evaluateLight(4, 1),partBFail(false,4,1));
+      assertEquals(true,test.evaluateLight(5, 4),partBFail(true,5,4));
    }
 
    private String partBFail(boolean expected, int num1, int num2){
